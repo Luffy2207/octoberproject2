894dabe38e15975d3230bcff6712a154caef9712
Luffy2207 <rahul140722@gmail.com>
2022-10-18 11:27:15 +0530

    loc to stage

---
+++ src/org/bank/AxisBank.java
@@ -0,0 +1,36 @@
+package org.bank;       //overriding //
+
+public class AxisBank extends BankInfo {
+
+public void deposit() {
+	System.out.println("Bank new Deposit amount is 180000");
+}
+	
+	public static void main(String[] args) {
+		AxisBank y =new AxisBank();
+		y.deposit();
+		y.savings();
+		y.fixed();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,13 @@
+package org.bank;         //Day 3- Task no:6   
+public class BankInfo {
+public void savings() {
+		System.out.println("Bank savings limt is 100000");
+	}
+	public void fixed() {
+		System.out.println("Bank Fixed amount is 200000");
+	}
+	
+	public void deposit() {
+		System.out.println("Bank minium Deposit is 150000 ");
+	}	
+	}
